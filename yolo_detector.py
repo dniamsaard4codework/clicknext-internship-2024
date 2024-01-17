6deaf80b90752c2ddbad51f5b8801bd555ab1432
Dechathon Niamsa-ard <dechathon_niamsa-ard@Dechathons-MacBook-Pro.local>
2024-01-18 05:38:05 +0700

    Update the AI internship technical test

--- yolo_detector.py
+++ yolo_detector.py
@@ -2,12 +2,15 @@ from ultralytics import YOLO
 from ultralytics.utils.plotting import Annotator, colors
 import cv2
 
-# Load YOLO model
-model = YOLO("yolov8a.pt")
+# Create a list to keep the previous center point for tracking line
+previous_point = []
 
+# Load YOLO model
+model = YOLO("yolov8n.pt")
 
 def draw_boxes(frame, boxes):
     """Draw detected bounding boxes on image frame"""
+    global class_id, class_name, coordinator, confidence
 
     # Create annotator object
     annotator = Annotator(frame)
@@ -17,10 +20,29 @@ def draw_boxes(frame, boxes):
         coordinator = box.xyxy[0]
         confidence = box.conf
 
-    # Draw bounding box
+    # Create blue-bounding-box on cat detection
+    if class_name == 'cat':
+        bounding_color = (255, 0, 0)
+    else:
+        bounding_color = colors(class_id, True)
+
+    # Draw bounding box on only cat
     annotator.box_label(
-        box=coordinator, label=class_name, color=colors(class_id, True)
+        box=coordinator, label=class_name, color=bounding_color
     )
+
+    # Set the variables for all values in coordinator
+    x1,y1,x2,y2 = coordinator
+
+    # Find the center of the box to make the plotting point
+    track_center = ((x1+x2)/2,(y1+y2)/2)
+    previous_point.append(track_center)
+
+    # Draw the tracking line for whole cat detection
+    for i in range(1, len(previous_point)):
+        start = (int(previous_point[i-1][0]), int(previous_point[i-1][1]))
+        end = (int(previous_point[i][0]), int(previous_point[i][1]))
+        cv2.line(frame, start, end, (81,255,81), 3)
 
     return annotator.result()
 
@@ -28,44 +50,42 @@ def draw_boxes(frame, boxes):
 def detect_object(frame):
     """Detect object from image frame"""
 
-    # Detect object from image frame
-    results = model.prediction(frame)
+    # Detect object from image frame (Detect only "cat" class)
+    results = model(frame, classes = 15)
 
     for result in results:
-    frame = draw_boxes(frame, result.boxes)
+        frame = draw_boxes(frame, result.boxes)
 
     return frame
 
 
 if __name__ == "__main__":
     video_path = "CatZoomies.mp4"
-    cap = cv.VideoCapture(video_path)
-
-    # Define the codec and create VideoWriter object
-    video_writer = cv.VideoWriter(
-        video_path + "_demo.avi", cv.VideoWriter_fourcc(*"MJPG"), 30, (1280, 720)
-    )
+    cap = cv2.VideoCapture(video_path)
 
     while cap.isOpened():
         # Read image frame
-        ret, frame = cap.read_frame()
+        ret, frame = cap.read()
 
         if ret:
             # Detect motorcycle from image frame
             frame_result = detect_object(frame)
-
-            # Write result to video
-            video_writer.write(frame_result)
-
             # Show result
-            cv.namedWindow("Video", cv.WINDOW_NORMAL)
-            cv.imshow("Video", frame_result)
-            cv.waitKey(30)
+            cv2.namedWindow("Video", cv2.WINDOW_NORMAL)
+            # Write text on the top-right corner of the video
+            cv2.putText(frame,  
+                'Dechathon-Clicknext-Internship-2024',  
+                (frame.shape[0]-60, 30),  
+                cv2.FONT_HERSHEY_SIMPLEX , 1,  
+                (0, 0, 255),  
+                2,  
+                cv2.LINE_4) 
+            cv2.imshow("Video", frame_result)
+            cv2.waitKey(30)
 
         else:
             break
 
     # Release the VideoCapture object and close the window
-    video_writer.release()
     cap.release()
     cv2.destroyAllWindows()
